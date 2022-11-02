69e15203ab648d3e34193d3317d4f6d8a6138bbc
Matheus Pavanetti <matheuspavanetti@gmail.com>
2022-11-02 16:37:44 -0600

    Adding spark python sample jobs.

---
+++ jobs/spark_test_job.py
@@ -0,0 +1,24 @@
+from pyspark.sql.session import SparkSession
+import os
+import logging
+
+if __name__ == "__main__":
+
+    spark = (SparkSession
+            .builder
+            .appName("TestApplicationLocal")
+            .getOrCreate())
+
+
+    logging.warning(f"Spark Session Created Object --> {spark}")
+    logging.warning(f"Spark Default CPU Cores --> {spark.sparkContext.defaultParallelism}")
+    
+    df = spark.read.csv("/home/jovyan/work/data/input/test.csv",header=True)
+    logging.warning(f"Dataframe partitions --> {df.rdd.getNumPartitions()}")
+    
+    df.printSchema()
+
+    df.show(20,False)
+    
+    df.write.mode("overwrite").csv("/home/jovyan/work/data/output/test_output")
+
---
+++ jobs/spark_test_job_cluster.py
@@ -0,0 +1,23 @@
+from pyspark.sql.session import SparkSession
+import os
+import logging
+
+if __name__ == "__main__":
+
+    spark = (SparkSession
+            .builder
+            .appName("TestApplicationCluster")
+            .master("spark://spark:7077")
+            .getOrCreate())
+
+
+    logging.warning(f"Spark Session Created Object --> {spark}")
+    logging.warning(f"Spark Default CPU Cores --> {spark.sparkContext.defaultParallelism}")
+    
+    df = spark.read.csv("/home/jovyan/work/data/input/test.csv",header=True)
+    logging.warning(f"Dataframe partitions --> {df.rdd.getNumPartitions()}")
+    
+    df.printSchema()
+
+    df.show(20,False)
+    
